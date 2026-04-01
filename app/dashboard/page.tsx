e912b2a281794751e8d76fab15a9e4163443ba4a
satriaasn <satriaasn@users.noreply.github.com>
2026-04-01 07:28:41 +0700

    fix: directive placement and naming collisions to fix vercel build

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,6 @@
-export const dynamic = 'force-dynamic';
 "use client";
+
+export const dynamic = 'force-dynamic';
 
 import { Sidebar } from "@/components/dashboard/sidebar";
 import { useEffect, useState } from "react";
